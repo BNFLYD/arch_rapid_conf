4a349ba8b56e995ed4de34911be0170f15d3b13a
BNFLYD <moralesflaviog@gmail.com>
2026-07-09 22:50:43 -0300

    fasfetch 3

--- fastfetch/helper.sh
+++ fastfetch/helper.sh
@@ -8,4 +8,12 @@ pick_logo() {
 
   local selected="${logos[$RANDOM%${#logos[@]}]}"
   ln -sf "$selected" "$logos_dir/current_logo"
+
+  # ─── Generar config.jsonc con el type correcto ───
+  if [[ "$selected" == *.txt ]]; then
+    local type="auto"
+  else
+    local type="kitty"
+  fi
+  sed "s/__LOGO_TYPE__/$type/" ~/.config/fastfetch/config.template.jsonc >~/.config/fastfetch/config.jsonc
 }
